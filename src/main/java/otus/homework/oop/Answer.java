d3a66e67a01fbc8e64ac12842c6550173e105923
Oleg K <just.oleg.it@gmail.com>
2022-11-04 18:40:21 -0400

    4th HomeWork

--- src/main/java/otus/homework/oop/Answer.java
+++ src/main/java/otus/homework/oop/Answer.java
@@ -2,8 +2,6 @@ package otus.homework.oop;
 
 public class Answer {
 
-
-    private Question question;
     private String text;
     private boolean isCorrect;
 
@@ -21,14 +19,6 @@ public class Answer {
 
     public boolean isCorrect() {
         return this.isCorrect;
-    }
-
-    public Question getQuestion() {
-        return this.question;
-    }
-
-    public void setQuestion(Question question) {
-        this.question = question;
     }
 
 }
