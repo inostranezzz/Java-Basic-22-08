42ad35764fcd0140579e81ac9159be6cde7e700d
Oleg K <just.oleg.it@gmail.com>
2022-11-13 06:02:58 -0500

    4th HomeWork

--- src/main/java/org/example/homework/oop/Question.java
+++ src/main/java/org/example/homework/oop/Question.java
@@ -1,13 +1,61 @@
 package org.example.homework.oop;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Question {
     private final String text;
-    private final String[] answers;
-    private final int validAnswer;
+    private final String validAnswer;
+    private final String[] answerOptions;
 
-    public Question(String text, String... answers) {
+
+    public Question(String text, String validAnswer, String... answerOptions) {
         this.text = text;
-        this.answers = answers;
+        this.validAnswer = validAnswer;
+        this.answerOptions = answerOptions;
     }
-    public boolean ask();
+
+    public void displayQuestionBlock() {
+        System.out.println(text);
+
+        for (int i = 0; i < answerOptions.length; i++){
+            System.out.println((i+1) + ". " + answerOptions[i]);
+        }
+    }
+    public boolean askAnswer() {
+        System.out.println("Укажите номер правильного ответа:");
+
+        int answerNumber = 0;
+        boolean isInt = false;
+        boolean isCorrectNumber = false;
+        while (!(isInt&isCorrectNumber))
+        {
+            Scanner scanner = new Scanner(System.in);
+            isInt = scanner.hasNextInt();
+            if (isInt){
+                answerNumber = scanner.nextInt();
+                if (answerNumber <=0 | answerNumber > Arrays.stream(answerOptions).count()){
+                    System.out.println("Такого номера ответа не существует. Попробуйте еще раз:");
+                }
+                else{
+                    isCorrectNumber = true;
+                }
+            }
+            else{
+                System.out.println("Вы ввели не номер ответа. Попробуйте еще раз:");
+            }
+        }
+
+        boolean isValidAnswer = false;
+        if (Objects.equals(answerOptions[answerNumber - 1], validAnswer)){
+            System.out.println("Правильно");
+            isValidAnswer = true;
+        }
+        else{
+            System.out.println("Неправильно");
+        }
+
+        return isValidAnswer;
+    }
 }
--- src/main/java/org/example/homework/oop/Questionnaire.java
+++ src/main/java/org/example/homework/oop/Questionnaire.java
@@ -1,84 +1,34 @@
 package org.example.homework.oop;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Questionnaire {
-    private Question[] questions;
-    private int validAnswerCount = 0;
-    private int displedAnswerNumber = 1;
+    private final Question[] questions;
+    private int validAnswerCount;
 
     public Questionnaire(Question[] questions) {
         this.questions = questions;
     }
 
     public void runTest(){
-
+        for (int i = 0; i < questions.length; i++) {
+            questions[i].displayQuestionBlock();
+            boolean answer = questions[i].askAnswer();
+            if (answer){
+                validAnswerCount++;
+            }
+            System.out.println();
+        }
+        System.out.println("Результат: правильно" + " " + validAnswerCount + ", неправильно " + (Arrays.stream(questions).count() - validAnswerCount));
     }
 
     public static void main(String[] args) {
-        Question question1 = new Question("В файл с каким расширением компилируется java-файл?", "cs", "java", "class", "exe");
-        Question question2 = new Question("С помощью какой команды git можно получить полную копию удаленного репозитория?", "commit", "push", "clone", "copy");
-        question2.setText("С помощью какой команды git можно получить полную копию удаленного репозитория?");
-        Question question3 = new Question();
-        question3.setText("Какой применяется цикл, когда не известно количество итераций?");
-        Answer answer31 = new Answer();
-        answer31.setText("while");
-        answer31.setCorrect(true);
-        Answer answer32 = new Answer();
-        answer32.setText("for");
-        Answer answer33 = new Answer();
-        answer33.setText("loop");
-        question3.setAnswers(new Answer[]{answer31, answer32, answer33});
+        Question question1 = new Question("В файл с каким расширением компилируется java-файл?", "class","cs", "java", "class", "exe");
+        Question question2 = new Question("С помощью какой команды git можно получить полную копию удаленного репозитория?", "clone","commit", "push", "clone", "copy");
+        Question question3 = new Question("Какой применяется цикл, когда не известно количество итераций?", "while","while", "for", "loop");
         Question[] questions = {question1, question2, question3};
         Questionnaire questionnaire = new Questionnaire(questions);
         questionnaire.runTest();
-
-
-        for (Question question : questions) {
-            System.out.println(question.getText());
-            for (Answer answer : question.getAnswers()) {
-                System.out.println(displedAnswerNumber++ + ". " + answer.getText());
-            }
-            System.out.println("Укажите номер правильного ответа:");
-
-            int answer = 0;
-            boolean isInt = false;
-            boolean isCorrectNumber = false;
-            while (!(isInt&isCorrectNumber)) {
-                Scanner scanner = new Scanner(System.in);
-                isInt = scanner.hasNextInt();
-                if (isInt){
-                    answer = scanner.nextInt();
-                    if (answer <= 0 | answer > question.getAnswers().length){
-                        System.out.println("Такого номера ответа не существует. Попробуйте еще раз:");
-                    }
-                    else{
-                        isCorrectNumber = true;
-                    }
-                }
-                else{
-                    System.out.println("Вы ввели не номер ответа. Попробуйте еще раз:");
-                }
-            }
-            Answer[] answers = question.getAnswers();
-            if (answers[answer-1].isCorrect()){
-                System.out.println("Правильно");
-                validAnswerCount++;
-            }
-            else{
-                System.out.println("Неправильно");
-            }
-
-            System.out.println();
-            displedAnswerNumber = 1;
-        }
-
-        long failAnswerCount = questions.length - validAnswerCount;
-        System.out.println("Результат: правильно" + " " + validAnswerCount + ", неправильно " + failAnswerCount);
-
-
-
-
     }
 
 }
