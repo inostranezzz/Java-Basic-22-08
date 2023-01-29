b272a2ab405fbcc4adc25cc02af216c0ee0186c5
inostranezzz <just.oleg.it@gmail.com>
2023-01-30 00:00:25 +0300

    refactor business logic

--- src/test/java/org/example/PrintCurrency/PrintCurrencyTest.java
+++ src/test/java/org/example/PrintCurrency/PrintCurrencyTest.java
@@ -1,10 +1,39 @@
 package org.example.PrintCurrency;
 
-import org.junit.Test;
+import org.example.homework.PrintCurrency.PrintCurrency;
+import org.junit.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 public class PrintCurrencyTest {
-    @Test
-    public void test(){
 
+    @After
+    public void cleanDownStreams() {
+        System.setIn(null);
+    }
+
+    @Test
+    public void testMain_withValidValue() {
+        InputStream stIn= System.in;
+        System.setIn(new ByteArrayInputStream("1".getBytes()));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(outputStream);
+        PrintStream stOut = System.out;
+        System.setOut(ps);
+
+        PrintCurrency.main(new String[0]);
+
+        System.setIn(stIn);
+        System.setOut(stOut);
+
+        String outputText = outputStream.toString();
+        String key = "Введите сумму в рублях:";
+        String result = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+
+        Assert.assertEquals("1 рубль", result.toString());
     }
 }
